7e8773659fa26d0f8df38aec08e50e9ec4bd3b8a
Aris van Ommeren <avommeren@vxcompany.com>
2020-04-08 01:19:28 +0200

    update voor blogpost

--- src/main/java/com/aristosvo/example/SampleApplication.java
+++ src/main/java/com/aristosvo/example/SampleApplication.java
@@ -1,5 +1,11 @@
 package com.aristosvo.example;
 
+import com.azure.core.credential.TokenCredential;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.storage.blob.models.BlobItem;
+import com.azure.storage.blob.specialized.BlockBlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,12 +18,29 @@ public class SampleApplication implements CommandLineRunner {
     @Value("${your.property.name}")
     private String mySecretProperty;
 
+    @Value("${blobcontainer.name:storage}")
+    private String name;
+ 
+    @Value("${blobcontainer.endpoint}")
+    private String endPoint;
+
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
 
     public void run(String... varl) throws Exception {
-        System.out.println("property yourSecretPropertyName in Azure Key Vault: " + mySecretProperty);
+        System.out.println("property your-property-name in Azure Key Vault: " + mySecretProperty);
+
+        TokenCredential tokenCredential = new ManagedIdentityCredentialBuilder().build();
+        BlobContainerClient containerClient = new BlobContainerClientBuilder()
+                    .endpoint(endPoint)
+                    .credential(tokenCredential)
+                    .containerName(name)
+                    .buildClient();
+
+        for (BlobItem blobItem : containerClient.listBlobs()) {
+            System.out.println("\t" + blobItem.getName());
+        }
     }
 
 }
